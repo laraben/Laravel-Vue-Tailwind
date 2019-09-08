ab49af34d63ef99ea6ba8064872a6a4b0f672b70
Benoit <Laraben@MacBook-Pro-van-Benoit.local>
2019-09-08 20:49:35 +0200

    vue-router

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Compressed tailwind css/sass -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <!-- Styles -->
 
     </head>
     <body>
@@ -23,6 +25,7 @@
                 </div>
             </div>
         </div>
+        <!-- Vue script -->
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
